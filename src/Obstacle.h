614f6afe2bf3008b630009497deef2027bb74d64
Ines Moital <ines.moital@hotmail.com>
2011-04-02 01:32:36 +0100

    Obstacles now can move

--- src/Obstacle.h
+++ src/Obstacle.h
@@ -9,29 +9,34 @@
 #ifndef OBSTACLE_H
 #define OBSTACLE_H
 
+#define MAX_MOV 10
+#define VELOCITY 0.1
+
 #include "cg.h"
 
 namespace Micromachines {
-    class Obstacle : public cg::Entity,
-    public cg::IDrawListener,
-    public cg::IUpdateListener{
-    private:
-        cg::Vector3d _position, _color;
-        double _winHeight, _winWidth;
-        int _type;
+	class Obstacle : public cg::Entity,
+	public cg::IDrawListener,
+	public cg::IUpdateListener
+	{
+	private:
+		int _type, _direction;
+		cg::Vector3d _position, _color, _initialPos;
+		double _winHeight, _winWidth;
+		bool _isMovable;
         
-        int RandomBeetween(int min, int max);
-        void drawCircle();
-        void drawRectangle();
-        void drawTriangle();
+		int RandomBeetween(int min, int max);
+		void drawCircle();
+		void drawRectangle();
+		void drawTriangle();
         
-    public:
-        Obstacle(std::string name);
-        ~Obstacle();
-        void init();
-        void draw();
-        void update(unsigned long elapsed_millis);
-    };
+	public:
+		Obstacle(std::string name);
+		~Obstacle();
+		void init();
+		void draw();
+		void update(unsigned long elapsed_millis);
+	};
 }
 
 #endif
