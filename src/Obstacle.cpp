f5637ef8056b87c0e3a854d8900fb5b77c86ad15
Ines Moital <ines.moital@hotmail.com>
2011-04-03 20:24:32 +0100

    Now obstacles are 3D and are only rectangles.

--- src/Obstacle.cpp
+++ src/Obstacle.cpp
@@ -15,51 +15,15 @@ namespace Micromachines {
 		return (rand() % (max-min) + min);
 	}
     
-	void Obstacle::drawCircle()
-	{
-		glBegin(GL_POLYGON);
-			glVertex3d(_position[0]-27, _position[1]+9, -400);       
-			glVertex3d(_position[0]-27, _position[1]-9, -400);       
-			glVertex3d(_position[0]-9, _position[1]-27, -400);       
-			glVertex3d(_position[0]+9, _position[1]-27, -400);      
-			glVertex3d(_position[0]+27, _position[1]-9, -400);       
-			glVertex3d(_position[0]+27, _position[1]+9, -400);       
-			glVertex3d(_position[0]+9, _position[1]+27, -400);       
-			glVertex3d(_position[0]-9, _position[1]+27, -400);       
-		glEnd();
-	}
-    
-	void Obstacle::drawRectangle()
-	{
-		glBegin(GL_POLYGON);
-			glVertex3d(_position[0]-27, _position[1]+18, -400);       
-			glVertex3d(_position[0]-27, _position[1]-18, -400);       
-			glVertex3d(_position[0]+27, _position[1]-18, -400);       
-			glVertex3d(_position[0]+27, _position[1]+18, -400); 
-		glEnd();
-	}
-    
-	void Obstacle::drawTriangle()
-	{
-		glBegin(GL_POLYGON);
-			glVertex3d(_position[0]-27, _position[1]-18, -400);       
-			glVertex3d(_position[0]+27, _position[1]-18, -400);       
-			glVertex3d(_position[0], _position[1]+18, -400);       
-		glEnd();
-	}
-    
 	Obstacle::Obstacle(std::string name) : cg::Entity(name){}
     
 	Obstacle::~Obstacle(){}
     
 	void Obstacle::init()
 	{
-		_type = RandomBeetween(0, 2);
-		printf("%d\n", _type) ;
 		_winWidth = cg::Manager::instance()->getApp()->getWindow().width;
 		_winHeight = cg::Manager::instance()->getApp()->getWindow().height;
 		_position = cg::Vector3d(RandomBeetween(27, (_winWidth-27)), RandomBeetween(27, (_winHeight-27)), -400);
-		printf("x = %f y = %f z = %f\n", _position[0], _position[1], _position[2]);
 		_color = cg::Vector3d(0.9, 0.9, 0.9);
 		int i = rand()%2;
 		if (i == 0) {
@@ -72,13 +36,44 @@ namespace Micromachines {
 	void Obstacle::draw()
 	{
 		glColor3d(_color[0], _color[1], _color[2]);
-		if (_type == 0) {
-			drawCircle();
-		}
-		else if(_type == 1){
-			drawRectangle();
-		}
-		else drawTriangle();
+		glBegin(GL_QUADS);
+		//top
+		glVertex3d(_position[0]-27, _position[1]+18, -400);       
+		glVertex3d(_position[0]-27, _position[1]-18, -400);       
+		glVertex3d(_position[0]+27, _position[1]-18, -400);       
+		glVertex3d(_position[0]+27, _position[1]+18, -400);
+		
+		//bottom
+		glVertex3d(_position[0]-27, _position[1]+18, -395);       
+		glVertex3d(_position[0]-27, _position[1]-18, -395);       
+		glVertex3d(_position[0]+27, _position[1]-18, -395);       
+		glVertex3d(_position[0]+27, _position[1]+18, -395);
+		
+		//side1
+		glVertex3d(_position[0]-27, _position[1]-18, -400);
+		glVertex3d(_position[0]+27, _position[1]-18, -400);
+		glVertex3d(_position[0]+27, _position[1]-18, -395);
+		glVertex3d(_position[0]-27, _position[1]-18, -395);
+		
+		//side2
+		glVertex3d(_position[0]+27, _position[1]-18, -400);
+		glVertex3d(_position[0]+27, _position[1]+18, -400);
+		glVertex3d(_position[0]+27, _position[1]+18, -395);
+		glVertex3d(_position[0]+27, _position[1]-18, -395);
+		
+		//side3
+		glVertex3d(_position[0]-27, _position[1]+18, -400);
+		glVertex3d(_position[0]+27, _position[1]+18, -400);
+		glVertex3d(_position[0]+27, _position[1]+18, -395);
+		glVertex3d(_position[0]-27, _position[1]+18, -395);
+		
+		//side4
+		glVertex3d(_position[0]-27, _position[1]-18, -400);
+		glVertex3d(_position[0]-27, _position[1]+18, -400);
+		glVertex3d(_position[0]-27, _position[1]+18, -395);
+		glVertex3d(_position[0]-27, _position[1]-18, -395);
+		
+		glEnd();
 	}
     
 	void Obstacle::update(unsigned long elapsed_millis) 
