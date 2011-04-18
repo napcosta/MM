981f932cb37c55950675920a1cba6677f6a1a912
shadowpt <nuno_costa@live.com>
2011-04-18 17:05:41 +0100

    last push was a mistake. Now the bug is corrected!!

--- src/Obstacle.cpp
+++ src/Obstacle.cpp
@@ -105,17 +105,16 @@ namespace Micromachines {
 		bool frontCollision;
 		
 		cg::Vector2d size = cg::Vector2d(27, 18);
+		if (_car->getVelocity() >= 0)
+			frontCollision = true;
+		else
+			frontCollision = false;
 		if (_car->isCollision(_position, size)) {
 			_car->decreaseLife();
-			if (_car->getVelocity() >= 0)
-				frontCollision = true;
-			else
-				frontCollision = false;
-			if (_car->getVelocity() >= 0 && frontCollision == true) {
+			if (_car->getVelocity() >= 0 && frontCollision == true)
 				_car->setVelocity(-1);
-			} else if (_car->getVelocity()<0 && frontCollision == false) {
-				_car->setVelocity(0.3);
-			}
+			else if (_car->getVelocity() <0 && frontCollision == false)
+				_car->setVelocity(0.1);
 		}
 	}
     
