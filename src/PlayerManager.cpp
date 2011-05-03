113d41521824c5446db06fc0e75510778be82de7
Ines Moital <ines.moital@hotmail.com>
2011-05-03 16:33:30 +0100

    WTF

---
+++ src/PlayerManager.cpp
@@ -0,0 +1,9 @@
+//
+//  PlayerManager.cpp
+//  cglib
+//
+//  Created by Inês on 5/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include "PlayerManager.h"
---
+++ src/PlayerManager.h
@@ -0,0 +1,7 @@
+//
+//  PlayerManager.h
+//  cglib
+//
+//  Created by Inês on 5/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
