e590a667a887e7d60be184dc002d7b2ef68307ec
Ines Moital <ines.moital@hotmail.com>
2011-05-03 16:34:30 +0100

    jhsfajg

--- src/PlayerManager.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  PlayerManager.cpp
-//  cglib
-//
-//  Created by Inês on 5/3/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
-#include "PlayerManager.h"
--- src/PlayerManager.h
+++
@@ -1,7 +0,0 @@
-//
-//  PlayerManager.h
-//  cglib
-//
-//  Created by Inês on 5/3/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
